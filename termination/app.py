5d7b193e1cf650513bf2c39f0bff4856628c37b5
Coding Cats <hectorman@crgs.co.uk>
2025-07-05 16:37:05 +0100

    Update app.py

--- termination/app.py
+++ termination/app.py
@@ -10,6 +10,10 @@ class TerminationApp(tk.Tk):
         self.title("Termination")
         self.geometry("600x400")
         self.configure(bg="black")
+
+        self.spinner_running = False
+        self.spinner_chars = itertools.cycle('|/-\\-')
+
         self.output_box = create_main_ui(self, self.handle_exit, self.execute_command)
 
     def handle_exit(self):
@@ -17,7 +21,22 @@ class TerminationApp(tk.Tk):
             self.destroy()
 
     def execute_command(self, cmd):
+        self.output_box.delete("1.0", tk.END)
+        self.spinner_running = True
+        self.update_spinner()
+        thread = threading.Thread(target=self.run_and_display, args=(cmd,))
+        thread.start()
+
+    def update_spinner(self):
+        if self.spinner_running:
+            next_char = next(self.spinner_chars)
+            self.output_box.delete("1.0", tk.END)
+            self.output_box.insert(tk.END, f"Running: {next_char}")
+            self.after(100, self.update_spinner)
+
+    def run_and_display(self, cmd):
         output = run_command(cmd)
+        self.spinner_running = False
         self.output_box.delete("1.0", tk.END)
         self.output_box.insert(tk.END, output)
 

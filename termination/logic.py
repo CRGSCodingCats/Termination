e0d501f2bf6b4b5c593bc3277b2d35a937b24a28
Coding Cats <hectorman@crgs.co.uk>
2025-07-09 18:04:28 +0100

    Update logic.py

--- termination/logic.py
+++ termination/logic.py
@@ -1,10 +1,21 @@
 from tkinter import messagebox
 import subprocess
+import os
 
 def confirm_exit():
     return messagebox.askyesno("Exit", "Do you wish to terminate?")
 
+def get_prompt():
+    cwd = os.getcwd()
+    venv_path = os.environ.get("VIRTUAL_ENV")
+    if venv_path:
+        env_name = os.path.basename(venv_path)
+        return f"({env_name}) TT {cwd}> "
+    else:
+        return f"TT {cwd}> "
+
 def stream_command(command):
+    yield get_prompt() + command + "\n"
     try:
         process = subprocess.Popen(
             command,
